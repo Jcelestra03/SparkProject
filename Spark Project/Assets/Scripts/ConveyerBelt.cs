cf54fb11928932ebf8fe04524109501ef57288a5
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-11-10 10:22:07 -0800

    modified Mya's convaer script and added place holder art

---
+++ Spark Project/Assets/Scripts/ConveyerBelt.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+    public float conveyerSpeed = 9.0f;
+
+    private SpriteRenderer SR;
+    private void Start()
+    {
+        transform.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.transform.position = Vector2.MoveTowards(collision.transform.position, transform.position * 1.5f, conveyerSpeed * Time.deltaTime);
+    }
+
+    void Animate()
+    {
+        
+    }
+}
