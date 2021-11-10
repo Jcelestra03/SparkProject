6b1a3d2f55e2021d5a2d33f7680cd75c8f17d98d
Jcelestra03 <82464830+Jcelestra03@users.noreply.github.com>
2021-11-10 12:14:12 -0800

    background script changes

--- Spark Project/Assets/Scripts/Tile Control/background.cs
+++ Spark Project/Assets/Scripts/Tile Control/background.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int height;
+    private int width;
+
+    bool[,] grid;
+
+    public void Init(int width, int height)
     {
-        
+        grid = new int[width, height];
+        this.width = width;
+        this.height = height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Set(int x, int y, bool to)
     {
-        
+        if(CheckPosition(x,y) == false) { return; }
+        {
+
+        }
+        grid[x, y] = to;
+    }
+
+    public int Get(int x, int y)
+    {
+        if(CheckPosition(x,y) == false)
+        {
+            return false;
+        }
+        return grid[x, y];
+    }
+
+    public bool CheckPosition(int x, int y)
+    {
+        if(x < 0 || x >= width)
+        {
+            return false;
+        }
+        if(y < 0 || y >= height)
+        {
+            return false;
+        }
+        return true;
     }
 }
