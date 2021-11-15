3ac1231ce12808bb3e20f9aa65b76fc03e564951
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-11-15 10:18:13 -0800

    Updated the player perfab and the player script
    I cleaned up the code with the player controller added a phew things and fixed the perfab i replaced its sprite with a basic whit one because the other one was glitching out.

--- Spark Project/Assets/Scripts/PlayerController.cs
+++ Spark Project/Assets/Scripts/PlayerController.cs
@@ -4,49 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float acceleration;
-    public float maxSpeed;
-    public float jumpheight = 6.5f;
-    public float groundDetectDistance = .1f;
+    public int health = 10;
+    public Transform checkpoint;
+
+    [SerializeField] private int respawns = 0;
+    [SerializeField] private int respawnHealth = 1;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpheight = 6.5f;
+
+    private float groundDetectDistance = .1f;
     private Rigidbody2D myRB;
     private Vector2 velocity;
     private Vector2 groundDetection;
-    public bool canjump;
+    private bool canjump;
 
-    public float timer;
-
-    // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        acceleration = maxSpeed / 2;
+        if (health <= 0 && respawns > 0 || health <= 0 && respawns == -1)
+            Respawn();
+        else if (health == 0 && respawns == 0)
+            Dead();
 
         velocity = myRB.velocity;
-        velocity.x += Input.GetAxisRaw("Horizontal") * acceleration;
+        velocity.x += Input.GetAxisRaw("Horizontal") * acceleration * Time.deltaTime;
 
         groundDetection = new Vector2(transform.position.x, transform.position.y - 1.1f);
-        if(canjump)
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics2D.Raycast(groundDetection, Vector2.down, groundDetectDistance))
         {
-            if (Input.GetKeyDown(KeyCode.Space) && Physics2D.Raycast(groundDetection, Vector2.down, groundDetectDistance))
-            {
-                velocity.y = jumpheight;
-                canjump = false;
-            }
+            velocity.y = jumpheight;
+            canjump = false;
         }
-        else if (!canjump)
-        {
-            timer += Time.deltaTime;
-            if ( timer >= 1.5)
-            {
-                canjump = true;
-                timer = 0;
-            }
-        }
+        
         if (velocity.x >= maxSpeed)
             velocity.x = maxSpeed;
 
@@ -59,5 +54,23 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // game over if no respawn marker present, else player respawn at nearest respawn marker
+    }
+
+    private void Respawn()
+    {
+        health = respawnHealth;
+        if (checkpoint == null)
+            transform.position = Vector2.zero;
+        else
+            transform.position = checkpoint.position;
+
+        if (respawns != -1)
+        respawns--;
+    }
+
+    private void Dead()
+    {
+        Debug.Log("dead");
+        //let the game manager know where dead
     }
 }
