85e3bfb3acca9f1845e221cc310198e1ff03a278
Superdude21 <82531612+Superdude21@users.noreply.github.com>
2021-11-17 13:43:45 -0800

    Canvas
    Added canvas for the UI

---
+++ Spark Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
