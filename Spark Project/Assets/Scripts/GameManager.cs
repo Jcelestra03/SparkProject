f965c871c2c94be7499da1d79d69a64d7f90a086
Superdude21 <82531612+Superdude21@users.noreply.github.com>
2021-11-18 13:37:40 -0800

    Game Manager
    Worked on some of the script for the UI

--- Spark Project/Assets/Scripts/GameManager.cs
+++ Spark Project/Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
-
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public Text starText;
+
+    int stars = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        starText.text = stars.ToString() + " Stars:";
     }
 
     // Update is called once per frame
-    void Update()
+
+    public void AddPoint()
     {
-        
+        stars += 1;
+        starText.text = stars.ToString() + " Stars:";
     }
 }
