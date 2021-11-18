faf7a736c1a1b2f38adc867c025c2c0e89a861fe
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-11-18 08:34:38 -0800

    Spikes up and running
    Script for spikes done
    Prefab for spikes done

--- Spark Project/Assets/Scripts/spike.cs
+++ Spark Project/Assets/Scripts/spike.cs
@@ -7,12 +7,31 @@ public class spike : MonoBehaviour
     public float nockBack = 5;
     public int damage = 1;
 
+    private float coolDown;
+
+    private void Update()
+    {
+        if (coolDown > 0)
+            coolDown -= 1 * Time.deltaTime;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && coolDown <= 0)
         {
             collision.transform.GetComponent<Rigidbody2D>().AddRelativeForce(-collision.relativeVelocity * nockBack);
             collision.transform.GetComponent<PlayerController>().health -= damage;
+            coolDown = 0.1f;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && coolDown <= 0)
+        {
+            collision.transform.GetComponent<Rigidbody2D>().AddRelativeForce(-collision.relativeVelocity * nockBack);
+            collision.transform.GetComponent<PlayerController>().health -= damage;
+            coolDown = 0.1f;
         }
     }
 }
