3f59b3cc4c45b25869bb471bc30438f97092d4fc
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-11-18 10:23:05 -0800

    working on upgrading the enemy controller
    in a unfinished state

--- Spark Project/Assets/Scripts/enemycontroller.cs
+++ Spark Project/Assets/Scripts/enemycontroller.cs
@@ -4,57 +4,62 @@ using UnityEngine;
 
 public class enemycontroller : MonoBehaviour
 {
+    public float attackSpeed = 10;
+    public float idleSpeed = 5;
 
+    private bool isfollowing = false;
+    private GameObject playertarget;
     private Rigidbody2D myRB;
     private Vector2 velocity;
-    public float movementSpeed = 2;
-    public bool isfollowing = false;
-    public GameObject playertarget;
+    private CircleCollider2D detector;
+    private int idleDir = -1;
 
-
-
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-        playertarget = GameObject.Find("player");
-
+        playertarget = GameObject.Find("Player");
+        detector = GetComponent<CircleCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 lookPos = playertarget.transform.position - transform.position;
-
         lookPos.Normalize();
         velocity = myRB.velocity;
-        if (!isfollowing)
-            velocity.x = 0;
+
+        if (!isfollowing) 
+            velocity.x = idleDir * (idleSpeed * 50) * Time.deltaTime;
+            
         if (isfollowing)
-        {
-            velocity.x = lookPos.x * movementSpeed;
-
-        }
+            velocity.x = lookPos.x * (attackSpeed * 50) * Time.deltaTime;
+            
         myRB.velocity = velocity;
-    }
 
+        if (!Physics2D.CircleCast(transform.position * new Vector2(-0.8f, -0.7f), 1, -transform.position))
+        {
+            gameObject.transform.localScale.Set(new Vector3(-1, 1, 1));
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!isfollowing && (collision.gameObject.name == "player"))
+        if (!isfollowing && (collision.gameObject.tag == "Player"))
+        {
             isfollowing = true;
-
+            detector.radius = 5;
+        }   
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (isfollowing && (collision.gameObject.tag == "Player"))
+        {
+            detector.radius = 2.5f;
+        } 
+    }
 
-        if (isfollowing && (collision.gameObject.name == "player"))
-            isfollowing = false;
-
+    private void Movement()
+    {
+        
     }
 }
