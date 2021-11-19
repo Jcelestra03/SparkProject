29e7ea6cc94e2baa820408a4a1ce65ea52476c89
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-11-19 10:20:34 -0800

    I worked on the broken enemy atie cliff fall sysum Help

--- Spark Project/Assets/Scripts/enemycontroller.cs
+++ Spark Project/Assets/Scripts/enemycontroller.cs
@@ -17,28 +17,38 @@ public class enemycontroller : MonoBehaviour
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-        playertarget = GameObject.Find("player");
+        playertarget = GameObject.Find("Player");
         detector = GetComponent<CircleCollider2D>();
     }
 
     void Update()
     {
+        //this is the broke boy i think it could also have to do somthing with the dirswop or ideal dir or just somthing in the movement systum with idle
+        if (!Physics2D.CircleCast(transform.position + new Vector3(0, -0.7f, 1), 0.1f, transform.position))
+        {
+            DirSwop();
+            Debug.Log("-+");
+        }
+    }
+
+    void FixedUpdate()
+    {
         Vector3 lookPos = playertarget.transform.position - transform.position;
         lookPos.Normalize();
         velocity = myRB.velocity;
 
-        if (!isfollowing) 
-            velocity.x = idleDir * (idleSpeed * 50) * Time.deltaTime;
-            
-        if (isfollowing)
-            velocity.x = lookPos.x * (attackSpeed * 50) * Time.deltaTime;
-            
-        myRB.velocity = velocity;
+        if (!isfollowing)
+            velocity.x = idleDir * idleSpeed * 50 * Time.deltaTime;
+        else
+            velocity.x = lookPos.x * attackSpeed * 50 * Time.deltaTime;
 
-        if (!Physics2D.CircleCast(transform.position * new Vector2(-0.8f, -0.7f), 1, -transform.position))
-        {
-            gameObject.transform.localScale = new Vector3(-1, 1, 1);
-        }
+        myRB.velocity = velocity;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position + new Vector3(0, -0.7f, 1), 0.1f) ;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -53,13 +63,17 @@ public class enemycontroller : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (isfollowing && (collision.gameObject.tag == "player"))
-        {
             detector.radius = 2.5f;
-        } 
     }
 
-    private void Movement()
+    private void DirSwop()
     {
-        
+        bool swop = false;
+        swop = !swop;
+
+        if (swop == true)
+            idleDir = 1;
+        else if (swop == false)
+            idleDir = -1;
     }
 }
