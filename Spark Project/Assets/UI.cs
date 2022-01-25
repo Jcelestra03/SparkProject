1342145fe06892aea2493d5341a7b804c2625b8e
Jeremiah Celestra <82464830+Jcelestra03@users.noreply.github.com>
2022-01-25 15:40:04 -0800

    Update UI.cs
    need to fix UI later

--- Spark Project/Assets/UI.cs
+++ Spark Project/Assets/UI.cs
@@ -11,11 +11,11 @@ public class UI : Selectable
     // Update is called once per frame
     void Update()
     {
-        if (IsHighlighted(m_BaseEvent) == true)
-        {
-            GameObject.Find("Main Camera").GetComponent<GridControl>().editing = false;
-        }
-        else
-            GameObject.Find("Main Camera").GetComponent<GridControl>().editing = true;
+        //if (IsHighlighted(m_BaseEvent) == true)
+        //{
+        //    GameObject.Find("Main Camera").GetComponent<GridControl>().editing = false;
+        //}
+        //else
+        //    GameObject.Find("Main Camera").GetComponent<GridControl>().editing = true;
     }
 }
