cf0622f17fffaa7b4f3c7796da7877b74637e34a
Jcelestra03 <82464830+Jcelestra03@users.noreply.github.com>
2022-02-18 12:34:03 -0800

    made a converybelt left

--- Spark Project/Assets/Scripts/ConveyerBelt.cs
+++ Spark Project/Assets/Scripts/ConveyerBelt.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class ConveyerBelt : MonoBehaviour
 {
     public float conveyerSpeed = 9.0f;
-
+    
     private void OnTriggerStay2D(Collider2D collision)
     {
-        collision.GetComponent<Rigidbody2D>().velocity += new Vector2(conveyerSpeed, 0);
+        if(gameObject.name.Contains("left"))
+            collision.GetComponent<Rigidbody2D>().velocity -= new Vector2(conveyerSpeed, 0);
+        else
+            collision.GetComponent<Rigidbody2D>().velocity += new Vector2(conveyerSpeed, 0);
     }
 }
